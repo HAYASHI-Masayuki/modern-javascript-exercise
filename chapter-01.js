38281096565bf75bcebadb51c8b8fdb36c0f40f5
HAYASHI Masayuki <masayuki.hayashi.mcc@gmail.com>
2022-10-09 16:13:03 +0900

    3-11問の解答を追加

--- chapter-01.js
+++ chapter-01.js
@@ -41,7 +41,93 @@ const questions = {
     // はできないため、NaN.
     const bracketsMinusBraces = [] - {}
     console.log('[] - {}', [] - {}, bracketsMinusBraces)
-  }
+  },
+
+  question3: () => {
+    [3, 2, 1, 0, -1, -2, -3].forEach(n => {
+      console.log(`${n} % 2 =`, n % 2)
+    });
+
+    [3, 2, 1, 0, -1, -2, -3].forEach(n => {
+      console.log(`${n} % -2 =`, n % -2)
+    });
+
+    [3.5, 2.5, 1.5, 0.5, -1.5, -2.5, -3.5].forEach(n => {
+      console.log(`${n} % 2 =`, n % 2)
+    });
+  },
+
+  question4: () => {
+    const addAngle = (n, o) => (n + o) % 360
+
+      console.log(`90 + 90 = `, addAngle(90, 90))
+      console.log(`180 + 180 = `, addAngle(180, 180))
+      console.log(`270 + 270 = `, addAngle(270, 270))
+      console.log(`360 + 360 = `, addAngle(360, 360))
+      console.log(`359 + 1 = `, addAngle(359, 1))
+  },
+
+  question5: () => {
+    console.log('\\\\')
+    console.log("\\\\")
+    console.log(`\\\\`)
+    console.log('\x5C\x5C')
+  },
+
+  question6: () => {
+    // raw
+    console.log('🌐')
+
+    // UTF-16
+    console.log('\uD83C\uDF10')
+
+    // UTF-32
+    console.log('\u{1F310}')
+  },
+
+  question7: () => {
+    const subpath = 'subpath';
+
+    console.log(`/path/to${subpath ? `/${subpath}` : ''}`)
+  },
+
+  question8: () => {
+    const array1 = []
+    array1[1] = 1
+    console.log(array1)
+
+    const array2 = []
+    array2.length = 1
+    console.log(array2)
+
+    console.log(Array(1))
+  },
+
+  question9: () => {
+    const array = []
+    array[0] = 0
+    array[0.5] = 0.5
+    array[1] = 1
+    array[1.5] = 1.5
+    array[2] = 2
+    console.log(array)
+    console.log(array.length)
+  },
+
+  question10: () => {
+    console.log([0, [1, 2]].toString())
+  },
+
+  question11: () => {
+    const harry = { friends: [] }
+    const sally = { friends: [] }
+    harry.friends.push(sally)
+    sally.friends.push(harry)
+    console.log(harry)
+    console.log(sally)
+    console.log(JSON.stringify(harry))
+    console.log(JSON.stringify(sally))
+  },
 }
 
 const [, , ...args] = process.argv.map(arg => `question${arg}`)
